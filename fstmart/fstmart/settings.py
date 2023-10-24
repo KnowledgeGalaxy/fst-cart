540e9ecb722af30c5b3fb2db9d76982598d83fb4
mdmainuddin360 <115131966+mdmainuddin360@users.noreply.github.com>
2023-10-24 13:32:44 +0530

    code updated

--- fstmart/fstmart/settings.py
+++ fstmart/fstmart/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'product',
     'rest_framework',
+    'corsheaders'
 
 ]
 
@@ -49,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+     'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'fstmart.urls'
@@ -127,3 +130,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+      'https://fstmart.up.railway.app',
+        'https://fstmart.up.railway.app/'  # Add the domains you want to allow
+]
