9d6c8bf5f2acfe023c3f0fb116ac540fd7d3feb2
mdmainuddin360 <115131966+mdmainuddin360@users.noreply.github.com>
2023-10-24 13:17:50 +0530

    code updated

--- fstmart/fstmart/settings.py
+++ fstmart/fstmart/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'product',
+    'rest_framework',
+
 ]
 
 MIDDLEWARE = [
@@ -74,8 +77,12 @@ WSGI_APPLICATION = 'fstmart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': '5LPTVZAr90dVlzG3hWD7',
+        'HOST': 'containers-us-west-98.railway.app',
+        'PORT': '7563',
     }
 }
 
