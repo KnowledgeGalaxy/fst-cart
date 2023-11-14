642c8a3549784794dd1c8ebac60680fb4ecb1659
mdmainuddin360 <115131966+mdmainuddin360@users.noreply.github.com>
2023-11-14 20:40:45 +0530

    code updated

--- fstmart/product/urls.py
+++ fstmart/product/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
 from .views import ProductListCreateAPIView, ProductDetailAPIView
-from .views import CategoryListCreateAPIView
+from .views import CategoryListCreateAPIView,CategoryDetailAPIView
 
 urlpatterns = [
     path('products/', ProductListCreateAPIView.as_view(), name='product-list-create'),
     path('products/<int:pk>/', ProductDetailAPIView.as_view(), name='product-detail'),
     path('categories/', CategoryListCreateAPIView.as_view(), name='category-list-create'),
-
+    path('categories/<int:pk>/', CategoryDetailAPIView.as_view(), name='category-detail'),
 ]
--- fstmart/product/views.py
+++ fstmart/product/views.py
@@ -47,6 +47,12 @@ class ProductDetailAPIView(APIView):
 
 
 
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Category
+from .serializers import CategorySerializer
+
 class CategoryListCreateAPIView(APIView):
     def get(self, request):
         categories = Category.objects.all()
@@ -59,3 +65,28 @@ class CategoryListCreateAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CategoryDetailAPIView(APIView):
+    def get(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        category = self.get_object(pk)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        category = self.get_object(pk)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    def get_object(self, pk):
+        try:
+            return Category.objects.get(pk=pk)
+        except Category.DoesNotExist:
+            raise Response(status=status.HTTP_404_NOT_FOUND)
